95429c3e996fa53fe62133e5e7f84afce5ab9870
HaoLiangPao <hflsforeverhao@gmail.com>
2022-03-01 13:23:59 -0500

    Routing changed for the website

--- src/components/homeComponent/Showcase.js
+++ src/components/homeComponent/Showcase.js
@@ -1,19 +1,23 @@
-import React from "react";
-import Typewritter from "./Typewritter";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Typewritter from './Typewritter';
 
 const Showcase = () => {
-  return (
-    <div className='header-content'>
-      <h1>
-        I Am Hao The
-        <Typewritter></Typewritter>
-      </h1>
-      <p className='lead'>I specialized in full-stack development.</p>
-      <a href='work.html' className='btn-light'>
-        View My Work
-      </a>
-    </div>
-  );
+	return (
+		<div className='header-content'>
+			<h1>
+				I Am Hao The
+				<Typewritter></Typewritter>
+			</h1>
+			<p className='lead'>I specialized in full-stack development.</p>
+			<Link to='/projects' className='btn-light'>
+				View My Work
+			</Link>
+			{/* <a href='work.html' className='btn-light'>
+				View My Work
+			</a> */}
+		</div>
+	);
 };
 
 export default Showcase;
